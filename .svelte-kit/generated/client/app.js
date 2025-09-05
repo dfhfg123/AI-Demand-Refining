0f474d481b4ca64d10c618ab98b4c4c88163b1e8
dfhfg123 <3247895009@qq.com>
2025-09-05 17:51:06 +0800

    fix

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -3,13 +3,25 @@ export { matchers } from './matchers.js';
 export const nodes = [
 	() => import('./nodes/0'),
 	() => import('./nodes/1'),
-	() => import('./nodes/2')
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
-		"/": [2]
+		"/": [2],
+		"/competitive": [3],
+		"/interview": [4],
+		"/meeting": [5],
+		"/prd": [6],
+		"/summarizer": [7],
+		"/tech-guide": [8]
 	};
 
 export const hooks = {
